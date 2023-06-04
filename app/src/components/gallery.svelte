1b035fa4b76da1f68a5fac3ddd0f84087f03ffc6
Radovan <55534148+Radovan30@users.noreply.github.com>
2023-06-04 23:56:07 +0200

    add photo gallery

--- app/src/components/gallery.svelte
+++ app/src/components/gallery.svelte
@@ -1,54 +1,70 @@
 <script>
-   import img from '../img/upload/2017_02.webp';
-   
+	import img1 from '../img/upload/2017_01.webp';
+	import img2 from '../img/upload/2017_02.webp';
+	import img3 from '../img/upload/2017_03.webp';
+	import img4 from '../img/upload/2017_04.webp';
+	import img5 from '../img/upload/2017_05.webp';
+	import img6 from '../img/upload/2017_06.webp';
+
+	let years = 2017;
 </script>
 
-<h1> ROČNÍK </h1>
-<div id="gallery" class="container mx-auto px-5 py-2 lg:px-32 lg:pt-24 md:mt-5">
-  <div class="-m-1 flex flex-wrap md:-m-2">
-    <div class="flex w-1/2 flex-wrap">
-      <div class="w-1/2 p-1 md:p-2">
-        <img
-          alt="gallery"
-          class="block h-full w-full rounded-lg object-cover object-center"
-          src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(70).webp" />
-      </div>
-      <div class="w-1/2 p-1 md:p-2">
-        <img
-          alt="gallery"
-          class="block h-full w-full rounded-lg object-cover object-center"
-          src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(72).webp" />
-      </div>
-      <div class="w-full p-1 md:p-2">
-        <img
-          alt="gallery"
-          class="block h-full w-full rounded-lg object-cover object-center"
-          src={img} />
-      </div>
-    </div>
-    <div class="flex w-1/2 flex-wrap">
-      <div class="w-full p-1 md:p-2">
-        <img
-          alt="gallery"
-          class="block h-full w-full rounded-lg object-cover object-center"
-          src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(74).webp" />
-      </div>
-      <div class="w-1/2 p-1 md:p-2">
-        <img
-          alt="gallery"
-          class="block h-full w-full rounded-lg object-cover object-center"
-          src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(75).webp" />
-      </div>
-      <div class="w-1/2 p-1 md:p-2">
-        <img
-          alt="gallery"
-          class="block h-full w-full rounded-lg object-cover object-center"
-          src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(77).webp" />
-      </div>
-    </div>
-  </div>
-</div>
+<article id="gallery" class="container mx-auto px-5 py-2 lg:px-30 lg:pt-4 mb-6">
+	<section>
+		<div>
+			<h1 class="text-xl font-bold text-center mt-4 mb-6">ROČNÍK {years}</h1>
+		</div>
+	</section>
+
+	<section class="-m-1 flex flex-wrap md:-m-2">
+		<div class="flex w-1/2 flex-wrap">
+			<div class="w-1/2 p-1 md:p-2">
+				<img
+					alt="gallery"
+					class="block h-full w-full rounded-lg object-cover object-center"
+					src={img2}
+				/>
+			</div>
+			<div class="w-1/2 p-1 md:p-2">
+				<img
+					alt="gallery"
+					class="block h-full w-full rounded-lg object-cover object-center"
+					src={img3}
+				/>
+			</div>
+			<div class="w-full p-1 md:p-2">
+				<img
+					alt="gallery"
+					class="block h-full w-full rounded-lg object-cover object-center"
+					src={img1}
+				/>
+			</div>
+		</div>
+		<div class="flex w-1/2 flex-wrap">
+			<div class="w-full p-1 md:p-2">
+				<img
+					alt="gallery"
+					class="block h-full w-full rounded-lg object-cover object-center"
+					src={img5}
+				/>
+			</div>
+			<div class="w-1/2 p-1 md:p-2">
+				<img
+					alt="gallery"
+					class="block h-full w-full rounded-lg object-cover object-center"
+					src={img4}
+				/>
+			</div>
+			<div class="w-1/2 p-1 md:p-2">
+				<img
+					alt="gallery"
+					class="block h-full w-full rounded-lg object-cover object-center"
+					src={img6}
+				/>
+			</div>
+		</div>
+	</section>
+</article>
 
 <style>
-    
 </style>
